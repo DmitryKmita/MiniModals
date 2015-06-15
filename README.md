e942c13c4638e810020010a71d0a7b946228f73d
Dmitry <dmitry@kmita.ru>
2015-06-16 00:19:27 +0300

    Library loaded

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # MiniModals
-A really small custom implementation of modals in JS.
+A really small custom implementation of modals in JS. Made for my personal needs, tried a lot of other scripts, but they are either too big, or do not have functionality I needed or was hard to implement easy things.
+
+I could make this library non-dependency (and maybe I'll do), but at this moment it needs jQuery, Font-Awesome to run.
+
+ How to run:
+ 1. Add jquery&font-awesome to your code.
+ 2. Import library to your code.
+ 3. Add library stylesheets (they can be rewritten as you wish, because they are very simple).
+ 4. To create simple modal with content - run modal.openModal(content),
+ 5. If you need to open modal and take content from ajax - run modal.openAjaxModal(url, data, trigger).
+ 5.1 URL - is the url for ajax request,
+ 5.2 DATA - is the data to pass to request
+ 5.3 TRIGGER - callback to initialize any events on elements loaded.
+ 6. If you need to close the modal - run modal.closeModal();
+
+I do not say that this implementation is good or bad. I just created it because needed fast solution for small functionality.
---
+++ source/js/modal.library.js
@@ -0,0 +1,72 @@
+/**
+ * Author: Dmitry Kmita <dmitry@kmita.ru>
+ *
+ * Under MIT Licence
+ *
+ * Custom Modal Library for easy modal usage
+ */
+var modal = {
+    currentContent: '',
+    openAjaxModal: function(url, data, trigger) {
+        var me = this;
+        me._sendRequest(url, data, function(response){
+            if (response.success) {
+                me.currentContent = response.content;
+                me.initModal();
+                trigger();
+            }
+        });
+    },
+    openModal: function(text) {
+        var me = this;
+        if (text !== undefined) {
+            me.currentContent = text;
+        }
+        me._initModal();
+        me._bindButtons();
+    },
+    _sendRequest: function (url, data, success) {
+        var request = $.ajax({
+            url: url,
+            type: "POST",
+            async: true,
+            data: data,
+            dataType: "json"
+        });
+        request.done(success);
+    },
+    _bindButtons: function () {
+        var me = this;
+        $("#modal-window .close").click(function () {
+            me.closeModal();
+        });
+    },
+    closeModal: function () {
+        var me = this;
+        me._removeWindow();
+        me._removeOverlay();
+    },
+    _initModal: function() {
+        var me = this;
+        me._initOverlay();
+        me._initWindow();
+    },
+    _initOverlay: function() {
+        if ($("#modal-overlay").length == 0) {
+            $("body").prepend('<div id="modal-overlay"></div>');
+        }
+    },
+    _removeOverlay: function() {
+        $("#modal-overlay").remove();
+    },
+    _initWindow: function() {
+        var me = this;
+        if ($("#modal-overlay").length == 0) {
+            me._initOverlay();
+        }
+        $("body").prepend('<div id="modal-window"><div class="close"><i class="fa fa-close"></i></div>'+me.currentContent+'</div>');
+    },
+    _removeWindow: function() {
+        $("#modal-window").remove();
+    }
+};
